152958ca87876e5da27f45590bda7f5653ecb732
kawsar uddin patoary <kawsaruddin450@gmail.com>
2024-10-24 00:06:12 +0600

    nested if

---
+++ nested_if.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    int number;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    if(number >= 0){
+        printf("Positive\n");
+        if(number %2 == 0){
+            printf("Even\n");
+        }
+        else{
+            printf("Odd.\n");
+        }
+    }else{
+        printf("Negative\n");
+    }
+}
