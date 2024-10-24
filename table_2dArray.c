27580675bbc66a4655967ff92724ea0e033241a3
kawsar uddin patoary <kawsaruddin450@gmail.com>
2024-10-24 19:47:25 +0600

    2d array to print table of 2 and 3

---
+++ table_2dArray.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+    int table[2][10];
+
+    for(int i=0; i<2; i++){
+        for(int j=0; j<10; j++){
+            if(i==0){
+                table[i][j] = 2*(j+1);
+            }else{
+                table[i][j] = 3*(j+1);
+            }
+
+            printf("%d\t", table[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
